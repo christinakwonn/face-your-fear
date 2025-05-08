a5daf654efead3d733f2d13da9408900f3fcb48d
stevensuu <ssu123@terpmail.umd.edu>
2025-05-08 01:31:40 -0400

    gorilla squish sound

--- Fear/Assets/GorillaColliderScript.cs
+++ Fear/Assets/GorillaColliderScript.cs
@@ -3,9 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class GorillaColliderScript : MonoBehaviour
 {
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip squishSound;
+    bool canRestart = false;
+    float restartTimer = 0;
+
+    private void OnEnable() {
+        restartTimer = 0;
+        canRestart = false;
+    }
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Gorilla"){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if (!canRestart){
+                if (audioSource != null)
+                    audioSource.PlayOneShot(squishSound);
+            }
+            canRestart = true;
+            
+        }
+    }
+    private void Update() {
+        if (canRestart){
+            restartTimer += Time.deltaTime;
+            if (restartTimer > 1.3f){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            
         }
     }
 }
